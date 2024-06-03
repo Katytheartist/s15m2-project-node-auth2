ec6944d780e99a4fcec2e97c5a153d3a6cfee17d
Katy Kane <katytheartist@gmail.com>
2024-06-02 23:34:43 -0500

    validaterolename

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -43,7 +43,20 @@ function findBy(filter) {
         "role_name": "admin",
       }
     ]
+    select
+      user_id,
+      username,
+      role_name,
+      password
+    from users
+    join roles on 
+      user.role_id=roles.role_id
+    where users.user_id=1;
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -57,6 +70,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
