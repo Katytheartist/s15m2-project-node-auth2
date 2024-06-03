a9f857e38e7db5db3900f5011abf907dbd6925a3
Katy Kane <katytheartist@gmail.com>
2024-06-02 23:01:30 -0500

    find usrmod

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -17,7 +17,17 @@ function find() {
         "role_name": "instructor"
       }
     ]
+    select
+user_id,
+username,
+role_name,
+from users
+join roles on 
+    user.role_id=roles.role_id;
    */
+  return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
